a69cb4a1eb73bbcd0385f3ed4c0e8af1ded16825
housman <theo.querment96@gmail.com>
2018-12-17 21:07:41 -0900

    Début script stats personnages

---
+++ MyLittleRPG/Assets/Scripts/Player/Attacks.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public abstract class Attacks : MonoBehaviour
+{
+    public string attackName;
+
+    public float forceFactor;
+    public float dextFactor;
+    public float intFactor;
+
+    public abstract void launchAttack(CharacterStats char1, CharacterStats char2);
+    public abstract void reussiteCritique(CharacterStats char1, CharacterStats char2);
+}
---
+++ MyLittleRPG/Assets/Scripts/Player/CharacterStats.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    //Force
+    public int force;
+    public int currentForce;
+    public int resForce;
+
+    //Dexterite
+    public int dext;
+    public int currentDext;
+    public int resDext;
+
+    //Inteligence
+    public int intel;
+    public int currentIntel;
+    public int resIntel;
+
+    //PV
+    public int PV;
+    public int currentPV;
+
+    //Attacks
+    public Attacks attack1;
+    public Attacks attack2;
+    public Attacks attack3;
+    public Attacks attack4;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
