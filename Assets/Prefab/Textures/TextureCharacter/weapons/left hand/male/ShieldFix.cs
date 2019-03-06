691891bb16e4d84dcb5aa4476f9df65b87fe1412
housman <theo.querment96@gmail.com>
2019-03-05 21:17:37 -0500

    texture enlevé du gitignore

---
+++ Assets/Prefab/Textures/TextureCharacter/weapons/left hand/male/ShieldFix.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ShieldFix : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animator anim = GetComponent<Animator>();
+        if (anim.GetCurrentAnimatorStateInfo(0).IsTag("Down"))
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            if (!anim.GetCurrentAnimatorStateInfo(0).IsTag("Transition"))
+                GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
