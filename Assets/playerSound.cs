aacb834f19a7557fa5e17eee4f6e1de85b09dadb
housman <theo.querment96@gmail.com>
2019-03-19 15:08:10 -0400

    bruit de marche

---
+++ Assets/playerSound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class playerSound : MonoBehaviour
+{
+
+    public AudioSource source;
+
+    public AudioClip[] stepClip;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void stepSound()
+    {
+        if (stepClip.Length > 0)
+        {
+            int clipIndex = Random.Range(0, stepClip.Length);
+            while (source.clip == stepClip[clipIndex])
+            {
+                clipIndex = Random.Range(0, stepClip.Length);
+            }
+            source.clip = stepClip[clipIndex];
+            source.Play();
+        }
+    }
+}
